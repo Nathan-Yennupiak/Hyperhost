6bc1d5bbbe5b8ec1e6e2c06cb8e7d1116aaf7974
Nathan-Yennupiak <nathanyennupiak1@gmail.com>
2024-06-17 20:04:08 +0000

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./build/**/*.{html,js}"],
   theme: {
     screens: {
       'sm': '640px',
